0aa839acc94697c3760a0adf51baa2515f0177d6
Nataly <nataly.turlevsky@conduit.com>
2013-07-15 17:22:04 +0300

    Authentication of users

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,5 @@
 class IdeasController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
 
   # GET /ideas
