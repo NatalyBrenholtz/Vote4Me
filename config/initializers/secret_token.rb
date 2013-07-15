730f310453feb3f11a85d6a41a913c9b0b5bc005
Nataly <nataly.turlevsky@conduit.com>
2013-07-15 11:35:49 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vote4Me::Application.config.secret_key_base = 'a96717821ae8a1ff7165cf15fdee652a9a12e7b8ba772cc938ebdbe571c24d1c3c65f6bfed76313994bacf2d83d090413f4545fc055d26147af9cd27bb55d363'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vote4Me::Application.config.session_store :cookie_store, key: '_Vote4Me_session'
